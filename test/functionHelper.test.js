6dbaabc5b175fff9460b5d9a21dcae10a77003d6
解大劦 <9727005@qq.com>
2021-11-03 22:28:13 +0800

    feature: 为函数添加通用柯里化的方式

---
+++ test/functionHelper.test.js
@@ -0,0 +1,48 @@
+const fh = require("../utils/functionHelper");
+const expect = require('chai').expect;
+
+
+describe("函数Helper测试", function () {
+
+    let aa = "this is a string!";
+    it("测试是否为function", function () {
+        expect(fh.isFunction(aa)).equals(false);
+    });
+
+    let ff = function () {
+    };
+    it("测试是否为function", function () {
+        expect(fh.isFunction(ff)).equals(true);
+    });
+
+    function myfunc1(a, b, c) {
+
+    }
+
+    it("测试参数的数量", function () {
+        expect(fh.getArgsCount(myfunc1)).equals(3);
+    });
+
+    function myfunc2(...args) {
+
+    }
+
+    it("测试参数的数量", function () {
+        expect(fh.getArgsCount(myfunc2)).equals(0);
+    });
+
+
+    let original = function (a, b, c, d) {
+        return a + b + c + d;
+    };
+    it('测试柯里化方法', function () {
+        let fa = fh.currying(original, 1, 2);
+        console.log(fa);
+        let fb = fa(3);
+        console.log(fb);
+        let fc = fb(4);
+        console.log(fc);
+        expect(fc).equals(10);
+    });
+});
+
--- utils/functionHelper.js
+++ utils/functionHelper.js
@@ -6,3 +6,46 @@ function isFunction(funcName) {
     }
 }
 
+/**
+ * 获取函数参数个数
+ * ────────────────────────
+ * 本方法是指该函数有多少个必须要传入的参数，即形参的个数。
+ * 形参的数量不包括剩余参数个数，仅包括第一个具有默认值之前的参数个数。
+ * @param funcName
+ * @returns {*}
+ */
+function getArgsCount(funcName) {
+    if (isFunction(funcName)) {
+        return funcName.length;
+    } else {
+        return 0;
+    }
+}
+
+/**
+ * @description: 将函数柯里化的工具函数
+ * @param {Function} fn 待柯里化的函数
+ * @param   args 已经接收的参数列表
+ * @return {Function}
+ */
+const currying = function (fn, ...args) {
+    // fn需要的参数个数
+    const len = fn.length
+    // 返回一个函数接收剩余参数
+    return function (...params) {
+        // 拼接已经接收和新接收的参数列表
+        let _args = [...args, ...params]
+        // 如果已经接收的参数个数还不够，继续返回一个新函数接收剩余参数
+        if (_args.length < len) {
+            return currying.call(this, fn, ..._args)
+        }
+        // 参数全部接收完调用原函数
+        return fn.apply(this, _args)
+    }
+}
+
+module.exports = {
+    isFunction,
+    getArgsCount,
+    currying,
+};
