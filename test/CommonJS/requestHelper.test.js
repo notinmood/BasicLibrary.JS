ef6012a23647e25d57004e526dacdf368870d3f1
解大劦 <9727005@qq.com>
2022-01-28 19:29:48 +0800

    refactor: 对服务器的请求改为使用 axios

---
+++ test/CommonJS/requestHelper.test.js
@@ -0,0 +1,9 @@
+/**
+ * @file   : requestHelper.test.js
+ * @time   : 19:24
+ * @date   : 2022/1/28
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
